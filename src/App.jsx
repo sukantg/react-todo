8bcfaf3104b8d56fd9b39742ff3f91a1352c33e9
Sukant Ghosh <sukantghosh@Sukants-MacBook-Air.local>
2024-11-07 22:20:23 +0000

    Add app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import TodoInput from "./components/todoInput"
+import TodoList from "./components/TodoList"
+
+function App() {
+
+  return (
+    <>
+    <main>
+      <TodoInput></TodoInput>
+      <TodoList></TodoList>
+    </main>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TodoInput() {
+
+    return  (
+        <div>
+            Todo Input
+        </div>
+    )
+}
+
+export default TodoInput;
